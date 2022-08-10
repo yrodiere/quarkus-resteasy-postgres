197a7c5a53c06c7132a908dab320417b48f2012a
Schaefer, Christian <C.Schaefer@t-systems.com>
2022-08-10 18:16:53 +0200

    further tests and comments

--- src/main/java/org/acme/PersonService.java
+++ src/main/java/org/acme/PersonService.java
@@ -1,14 +1,19 @@
 package org.acme;
 
 
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.hibernate.orm.panache.Panache;
 import org.acme.entity.Person;
+import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.transaction.Transactional;
 
 @ApplicationScoped
 public class PersonService {
+    @Inject
+    Logger log;
+
     @Transactional
     public void createPerson(String firstname) {
         Person p = new Person();
@@ -25,7 +30,26 @@ public class PersonService {
 
     @Transactional
     public void findAndUpdatePerson(Person personArg, String newFirstname){
+        //FIXME shouldnt make this personArg managed again? "When using JPA, to reassociate a detached entity to an active EntityManager, you can use the merge operation."
+        // https://stackoverflow.com/questions/912659/what-is-the-proper-way-to-re-attach-detached-objects-in-hibernate/60661154#60661154
+        Panache.getEntityManager().merge(personArg);
+        log.info("PanacheEntityBase.getEntityManager().contains(personArg)="+ Panache.getEntityManager().contains(personArg));
         Person person = Person.findById(personArg.id);
+        log.info("PanacheEntityBase.getEntityManager().contains(person)="+Panache.getEntityManager().contains(person));
+
         person.firstname = newFirstname;
+    }
+
+    // As long as we work with entites with transaction they keep managed and updates are working
+    // As soon as we exit a transaction boundary, the entitymananger is cleared and closed, entities get detached
+    // Only way to re-attach them/get them back in managed state seems to be a find
+    @Transactional
+    public void findAndUpdatePerson2(Long personId, String newFirstname){
+        Person person = findPerson(personId);
+        person.firstname = newFirstname;
+    }
+
+    public Person findPerson(Long personId){
+        return Person.findById(personId);
     }
 }
--- src/test/java/org/acme/PersonServiceTest.java
+++ src/test/java/org/acme/PersonServiceTest.java
@@ -1,5 +1,6 @@
 package org.acme;
 
+import io.quarkus.narayana.jta.QuarkusTransaction;
 import io.quarkus.test.junit.QuarkusTest;
 import org.acme.entity.Person;
 import org.junit.jupiter.api.Test;
@@ -23,6 +24,7 @@ public class PersonServiceTest {
         String firstname = "Max";
         String newFirstname = "Paul";
         String newFirstname2 = "Paul2";
+        String newFirstname3 = "Paul3";
 
         service.createPerson(firstname);                    // Person created, firstname = 'Max', created in DB after this line
         Person person = Person.findById(1L);                // returns created Person
@@ -35,7 +37,7 @@ public class PersonServiceTest {
         Person personUpdated = Person.findByIdForce(1L);    // clear cached entity to force query of db to get updated Person
         assertEquals(newFirstname, personUpdated.firstname);
 
-        personUpdated.updateFirstname(newFirstname2);       // FIXME person firstname not updated in db
+//        personUpdated.updateFirstname(newFirstname2);       // FIXME person firstname not updated in db
         service.updatePerson(personUpdated, newFirstname2); // FIXME person firstname not updated in db
                                                             // firstname = 'Paul2' set in entity, but NOT persisted in DB, so it is not working as described in the docu
                                                             // in  'https://quarkus.io/guides/hibernate-orm-panache#most-useful-operations' by:
@@ -43,6 +45,7 @@ public class PersonServiceTest {
                                                             // modifications are automatically persisted on transaction commit."
 
         service.findAndUpdatePerson(personUpdated, newFirstname2); // person firstname now finally updated in db
+        service.findAndUpdatePerson2(1L, newFirstname3); // person firstname now finally updated in db
 
         String s="";
     }
