d0d485add08f19c9e98efa83b4a00b3076adec7a
CS <syrious3000@yahoo.de>
2023-06-07 20:54:24 +0200

    copyApplicationPropertiesToSystemProperties

--- src/test/java/org/acme/FlywayTest.java
+++ src/test/java/org/acme/FlywayTest.java
@@ -4,12 +4,13 @@ import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
-@QuarkusTest
-@QuarkusTestResource(value = FlywayTestResource.class, restrictToAnnotatedClass = true)
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@QuarkusTest
+@QuarkusTestResource(FlywayTestResource.class)
 public class FlywayTest {
     @Test
     public void test() {
-        String s="";
+        assertTrue(true);
     }
 }
